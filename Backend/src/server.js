f1520c3bc0a838e3426fc1251679a642c2e84d5a
v7wed <v7wedv@gmail.com>
2025-12-10 18:25:57 +0400

    Ready for deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,22 +4,34 @@ import { connectDB } from "./Config/db.js";
 import dotenv from "dotenv";
 import theLimiter from "./Middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 //middleware
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
+
 app.use(express.json());
 app.use(theLimiter);
 
 app.use("/api/notes/", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Frontend/dist"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
